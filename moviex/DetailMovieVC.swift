c27b7af3cc1db4240cad90207ab9043e4a953e44
FrenchTastic <romainpardon@MacBook-Pro-de-Romain.local>
2016-08-07 22:32:24 +0200

    Correct animation
    Correct view disappear animation and a color for links.

--- moviex/DetailMovieVC.swift
+++ moviex/DetailMovieVC.swift
@@ -23,6 +23,7 @@ class DetailMovieVC: UIViewController {
         super.viewDidLoad()
         print(movie.title)
         movieImg.image = movie.getMovieImage()
+        movieImg.clipsToBounds = true
         movieTitle.text = movie.title
         movieDesc.text = movie.desc
         imdbUrl.text = movie.imdbUrl
@@ -33,16 +34,4 @@ class DetailMovieVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
